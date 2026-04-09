84f3aa9683eddb011242b45c4f05f2ea629daa46
k21971 <keith.simpson1971@gmail.com>
2026-04-09 23:30:16 +0000

    Spring 2026.

--- src/end.c
+++ src/end.c
@@ -548,7 +548,7 @@ dump_everything(
     init_symbols(); /* revert to default symbol set */
 
     /* let folks know this was a nethackathon game */
-    Sprintf(pbuf, "NetHackathon, Spring 2025");
+    Sprintf(pbuf, "NetHackathon, Spring 2026");
     putstr(0, ATR_SUBHEAD, pbuf);
     putstr(NHW_DUMPTXT, 0, "");
 
